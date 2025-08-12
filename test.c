664711fb8b2b62315517af4aa9ae949902782416
Ruan C. Keet <rckeet@gmail.com>
2025-08-12 08:17:44 +0200

    minor changes

--- test.c
+++ test.c
@@ -5,12 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#if defined(__linux__)
-#define SIZE_T_FMT "%lu"
-#elif defined(_WIN32)
-#define SIZE_T_FMT "%llu"
-#endif
-
 int main(void)
 {
     printf("Creating arena...\n");
@@ -22,7 +16,7 @@ int main(void)
     }
     rkDebugArena(arena);
 
-    printf("Allocating "SIZE_T_FMT" bytes...\n", sizeof(int) * 5);
+    printf("Allocating %zu bytes...\n", sizeof(int) * 5);
     int *const arr = rkArenaAlloc(arena, sizeof(int) * 5);
     if (!arr)
     {
